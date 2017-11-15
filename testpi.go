b9aac1283ec962e30b54e14d264cb7211a566f8d
Andrew Lee <leeas@stolaf.edu>
2017-11-15 06:49:17 -0600

    yolo

--- testpi.go
+++ testpi.go
@@ -8,9 +8,13 @@ import (
 )
 
 func main() {
+	// t := time.Now()
+	// currenttime := t.Format("15:04")
 	c := gron.New()
+	//Run the following once a minute
+	//Check all 4 alarms to see if the current time matches any configurations
 	c.AddFunc(gron.Every(1*time.Minute), func() {
-		fmt.Println("runs every minute.")
+		fmt.Println("YOLO")
 	})
 	c.Start()
 }
