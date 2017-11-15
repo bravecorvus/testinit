9e2f5eaaada27f1b059aeb972be36645c0c79d57
Andrew Lee <leeas@stolaf.edu>
2017-11-15 06:34:21 -0600

    yolo

--- testpi.go
+++ testpi.go
@@ -14,7 +14,7 @@ func main() {
 	//Run the following once a minute
 	//Check all 4 alarms to see if the current time matches any configurations
 	c.AddFunc("0 * * * * *", func() {
-		if currenttime == currenttime {
+		if currenttime == "6:35" {
 			fmt.Println("YOLO")
 
 		}
